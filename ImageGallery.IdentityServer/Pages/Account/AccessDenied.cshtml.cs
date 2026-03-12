2aec92dcdda6bb66993540141cda8b93ebaad7e3
Paolo Paci <paolopci@yahoo.it>
2026-03-12 16:11:09 +0100

    add IdentityServer QuickStart UI

---
+++ ImageGallery.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageGallery.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
